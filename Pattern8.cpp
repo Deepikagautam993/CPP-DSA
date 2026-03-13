7ce1eb251532545003d415e0e743393d6bc7d883
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-03-13 13:10:56 +0530

    Add files via upload

---
+++ Pattern8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int i = 1;
+	while(i <= n){
+		int j = 1;
+		while(j <= n){
+			cout << 'A'+ i - 1 << " ";
+			j++;
+		}
+		i++;
+		cout << endl;
+	}
+	return 0;
+}

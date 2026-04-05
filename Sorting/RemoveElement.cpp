63baa5588893738dcc0cdd686349497d4912d384
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-04-05 12:17:03 +0530

    Add files via upload

---
+++ Sorting/RemoveElement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+
+    int k = 0;
+
+    for(int i = 0; i < nums.size(); i++) {
+
+        if(nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+       }
+    }
+
+    return k;
+}
+
+int main(){
+	
+	vector<int> nums = {3, 2, 2, 3};
+	int val = 3;
+	cout << removeElement(nums,val) << endl;
+}

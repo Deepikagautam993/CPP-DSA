fbf25bcaa456d11d1b1a78394597c72772028704
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-05-07 21:56:44 +0530

    Add files via upload

---
+++ String/ValidAnagram.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+
+    if(s.size() != t.size()) {
+        return false;
+    }
+
+    vector<int> freq(26, 0);
+
+    for(int i = 0; i < s.size(); i++) {
+        freq[s[i] - 'a']++;
+        freq[t[i] - 'a']--;
+    }
+
+    for(int count : freq) {
+        if(count != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+
+    string s = "anagram";
+    string t = "nagaram";
+
+    if(isAnagram(s, t)) {
+        cout << "True";
+    }
+    else {
+        cout << "False";
+    }
+
+    return 0;
+}

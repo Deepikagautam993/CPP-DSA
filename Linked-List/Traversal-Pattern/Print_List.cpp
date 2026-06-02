da89eeb90115a0440b4def2ef3c608ac497ee29d
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-06-02 10:04:33 +0530

    Add files via upload

---
+++ Linked-List/Traversal-Pattern/Print_List.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void printList(Node* head)
+{
+    Node* temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node* node1 = new Node(1);
+    Node* node2 = new Node(2);
+    Node* node3 = new Node(3);
+    Node* node4 = new Node(4);
+
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+
+    Node* head = node1;
+
+    printList(head);
+
+    return 0;
+}
